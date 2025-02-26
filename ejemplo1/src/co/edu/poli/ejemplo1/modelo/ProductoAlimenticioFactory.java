a57ad1ab8f5bca7f9578c982a170bcbff67a10ec
Deivid380 <164451678+Deivid380@users.noreply.github.com>
2025-02-25 23:44:54 -0500

    Create ProductoAlimenticioFactory.java

---
+++ ejemplo1/src/co/edu/poli/ejemplo1/modelo/ProductoAlimenticioFactory.java
@@ -0,0 +1,17 @@
+package co.edu.poli.ejemplo1.modelo; 
+
+public class ProductoAlimenticioFactory extends ProductoFactory {
+
+    private String fechaExpiracion;
+
+    public ProductoAlimenticioFactory(String fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    @Override
+    public Producto crearProducto(String idProducto, String descripcion) {
+        Producto p = new ProductoAlimenticio(idProducto, descripcion, fechaExpiracion);
+        registrarProducto(p); // Usa el método de la clase abstracta
+        return p;
+    }
+}
