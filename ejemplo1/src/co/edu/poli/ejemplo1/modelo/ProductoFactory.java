94adb61c4c29ec38df9dbcbf55eaaf8651c23a90
JDteufel <149896541+JDteufel@users.noreply.github.com>
2025-03-11 13:48:24 -0500

    Delete ejemplo1 directory

--- ejemplo1/src/co/edu/poli/ejemplo1/modelo/ProductoFactory.java
+++
@@ -1,9 +0,0 @@
-package co.edu.poli.ejemplo1.modelo;
-
-public interface ProductoFactory {
-    
-    // Método abstracto que cada fábrica debe implementar
-    public Producto crearProducto(String idProducto, String descripcion);
-    
-    }
-}
