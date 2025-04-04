b458cd72e0024b58cdf76ddb1424c8eae19cb2e6
hillarylivR <Hlrodriguez@poligran.edu.co>
2025-04-04 16:40:38 -0500

    Lista de precios

--- ejemplo1/src/main/java/co/edu/poli/ejemplo1/modelo/CompraNormal.java
+++ ejemplo1/src/main/java/co/edu/poli/ejemplo1/modelo/CompraNormal.java
@@ -1,23 +1,20 @@
 package co.edu.poli.ejemplo1.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompraNormal implements Carrito {
-    double precio1, precio2, precio3, precio4, precio5, precio6, precio7, precio8, precio9, precio10;
+    private List<Double> precios;
 
     public CompraNormal() {
-        precio1 = 1000.0;
-        precio2 = 2000.0;
-        precio3 = 3000.0;
-        precio4 = 4000.0;
-        precio5 = 5000.0;
-        precio6 = 6000.0;
-        precio7 = 7000.0;
-        precio8 = 8000.0;
-        precio9 = 9000.0;
-        precio10 = 10000.0;
+        precios = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            precios.add(i * 1000.0);
+        }
     }
 
     @Override
     public double FinalizarCompra() {
-        return precio1 + precio2 + precio3 + precio4 + precio5 + precio6 + precio7 + precio8 + precio9 + precio10;
+        return precios.stream().mapToDouble(Double::doubleValue).sum();
     }
 }
