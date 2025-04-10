1e9d888f58544bfec1a90ce5648d0463e966aa25
hillarylivR <Hlrodriguez@poligran.edu.co>
2025-04-09 22:36:54 -0500

    Producto y Cliente

---
+++ ejemplo2/src/main/java/co/edu/poli/ejemplo2/controlador/ControladorInicio.java
@@ -0,0 +1,24 @@
+package co.edu.poli.ejemplo2.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ControladorInicio {
+
+    @FXML
+    private ListView<?> listPedidos;
+
+    @FXML
+    private TextField txtDescrip;
+
+    @FXML
+    private TextField txtIdC;
+
+    @FXML
+    private TextField txtIdP;
+
+    @FXML
+    private TextField txtNombre;
+
+}
---
+++ ejemplo2/src/main/java/co/edu/poli/ejemplo2/modelo/Producto.java
@@ -0,0 +1,40 @@
+package co.edu.poli.ejemplo2.modelo;
+
+public abstract class Producto {
+    private String idProducto;
+    private String descripcion;
+
+    public Producto(String idProducto, String descripcion) {
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+    }
+
+    protected Producto(Producto otro) {
+        this.idProducto = otro.getIdProducto();
+        this.descripcion = otro.getDescripcion();
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id='" + idProducto + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
