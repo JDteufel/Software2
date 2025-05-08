d0adfab05fdf7988de9a4abc841ffac20ce0cd50
JDteufel <jgomezcardenas30@gmail.com>
2025-05-07 21:04:45 -0500

    Corrección module-info

--- ejemplo1/src/main/java/module-info.java
+++ ejemplo1/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module co.edu.poli.ejemplo1 {
 	requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires transitive javafx.graphics;
 
     opens co.edu.poli.ejemplo1.vista to javafx.fxml;
     opens co.edu.poli.ejemplo1.controlador to javafx.fxml;
